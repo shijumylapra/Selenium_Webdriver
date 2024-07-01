52b0d67eb49b6dd57edb09d34d4a2f735e6baeb4
Shiju <shijumylapra@gmail.com>
2024-06-30 23:36:57 -0600

    create new WebElement for HSBC.co.in

---
+++ selenium_webdriver/src/test/java/webdriver/HSBC_Login_Test.java
@@ -0,0 +1,13 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HSBC_Login_Test {
+    public static void main(String[] args){
+        WebElement username,passwd,button_click,SignUp;
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hsbc.co.in/");
+    }
+}
